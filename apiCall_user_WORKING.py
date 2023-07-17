2d78fcbda31029d1f30969151f0890ede011eee4
DatadudeDev <132722687+DatadudeDev@users.noreply.github.com>
2023-07-16 23:23:18 -0700

    Delete apiCall_user_WORKING.py

--- apiCall_user_WORKING.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/execute_script'
-files = {'script': open('apiDev.py', 'rb')}
-data = {'user':'Gabriel'}
-
-response = requests.post(url, files=files, data=data)
-
-print(response.text)
