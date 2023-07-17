be1f1dee2eb5d08e3fddb40233c2758f28b54fd9
DatadudeDev <132722687+DatadudeDev@users.noreply.github.com>
2023-07-16 23:17:58 -0700

    Update apiCall.py

--- apiCall.py
+++ apiCall.py
@@ -1,8 +1,9 @@
 import requests
 
-url = 'http://localhost:5000/execute_script'
+url = 'http://localhost:1213/execute_script'
 files = {'script': open('loop.py', 'rb')}
+data = {'user':'Gabriel','poll':'Poll'}
 
-response = requests.post(url, files=files)
+response = requests.post(url, files=files, data=data)
 
 print(response.text)
