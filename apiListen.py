729db9ea8fa1068e90aa057ac4dacb4204635556
DatadudeDev <132722687+DatadudeDev@users.noreply.github.com>
2023-07-18 21:56:30 -0700

    Update apiListen.py

--- apiListen.py
+++ apiListen.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, send_file
 import subprocess
 
 app = Flask(__name__)
@@ -14,32 +14,48 @@ class ApiListener:
         raw_data = request.get_data()
         print(f'Raw request data: {raw_data.decode()}')
         
-        try:
-            # Parse 'user' and 'poll' from the JSON payload
-            data = request.json
-            user = data.get('user')
-            poll = data.get('poll')
-            print(f'user: {user}')
-            print(f'poll: {poll}')
-            
-            # Save user data to a JSON file
-            with open('user_data.json', 'w') as f:
-                json.dump(data, f)
-        except Exception as e:
-            print(f'Error parsing JSON: {e}')
-            import traceback
-            traceback.print_exc()
-            return 'Error parsing JSON', 400
+        poll = request.json.get('poll')
+        answer1 = request.json.get('answer1')
+        answer2 = request.json.get('answer2')
+        answer3 = request.json.get('answer3')
+        answer4 = request.json.get('answer4')       
+        #print(f'user: {user}')
+        #print(f'poll: {poll}')
+        
+        # Save user data to a JSON file
+        data = {'prompt':poll,'answer1':answer1,'answer2':answer2,'answer3':answer3,'answer':answer4}
+        with open('prompt.json', 'w') as f:
+            json.dump(data, f)
             
         # Execute the Python script as a subprocess
-        result = subprocess.run(['python', 'apiDev.py'], capture_output=True, text=True)
-        
-        if result.returncode == 0:
-            output = result.stdout
-            return f'Script executed successfully.\nOutput:\n{output}'
-        else:
-            error = result.stderr
-            return f'Script execution failed.\nError:\n{error}'
+        try:
+            result = subprocess.run(['python', 'gen.py'], capture_output=True, text=True, check=True)
+            
+            # Read the content of image_url.json created by gen.py
+            with open('image_url.json', 'r') as f:
+                url_json = json.load(f)
+            
+            # Return the content of image_url.json as the API response
+            return json.dumps(url_json)
+            
+        except subprocess.CalledProcessError as e:
+            error = e.stderr.strip()
+            return f'Script execution failed.\nError:\n{error}', 500
+
+    @app.route('/get_json_file', methods=['GET'])
+    def get_json_file():
+        raw_data = request.get_data()
+        print(f'Raw GET request data: {raw_data.decode()}')
+        try:
+            # Read the content of image_url.json
+            with open('image_url.json', 'r') as f:
+                url_json = json.load(f)
+
+            # Return the JSON file as the response
+            print(url_json)
+            return url_json
+        except FileNotFoundError:
+            return 'JSON file not found.', 404
 
     def run(self, host='0.0.0.0', port=1213):
         app.run(host=host, port=port)
