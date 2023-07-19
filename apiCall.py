b9e03c973bf9f501d391ee7007f47c4f97279925
DatadudeDev <132722687+DatadudeDev@users.noreply.github.com>
2023-07-18 21:57:26 -0700

    Update apiCall.py

--- apiCall.py
+++ apiCall.py
@@ -1,8 +1,8 @@
 import requests
 
 url = 'https://pc.datadude.dev/execute_script'
-data = {'user':'Gabriel','poll':'Poll'}
+data = {'poll':'who should be president in 2024?','answer1': 'Donald Trump', 'answer2': 'Barack Obama','answer3':'Joe Biden','answer4':'Xi Xinping'}
 
-response = requests.post(url, data=data)
+response = requests.post(url, json=data)  # Use json parameter here
 
 print(response.text)
